30bfb7554f27a1eafc7773cf0f3df4aab12cbf6b
LILITH-SERVER\server <lilithbe.dev@gmail.com>
2022-05-17 23:48:58 +0900

    dddd

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,9 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import rootReducer from './rootReducer'
-
+const isDev= process.env.REACT_APP_TYPE==='development'?true:false
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(logger,thunk))
+    isDev?composeWithDevTools(applyMiddleware(logger,thunk)):applyMiddleware(thunk)
 )
 export default store
